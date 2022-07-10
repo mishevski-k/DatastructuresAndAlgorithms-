f0a9ba55ec215e2a26a3406c619d9ca664afaf12
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-07-10 19:23:35 +0200

    Added Selection Sort algoritham and example

---
+++ SelectionSort/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        //  Selection Sort: search through an array and keep track of the minimum value during each iteration
+        //  ,At the end of each iteration we swap variables
+        //
+        //  Quadratic time O(n^2)
+        //  Small data set = Okay-ish
+        //  Large data set = bad
+
+
+        int array[] = {8, 7, 9, 2, 3, 1, 5, 4, 6};
+
+        selectionSort(array);
+
+        for(int i : array){
+            System.out.println(i + ", ");
+        }
+    }
+
+    private static void selectionSort(int[] array){
+        for(int i = 0; i < array.length - 1; i++){
+            int min = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[min] > array[j]){
+                        min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+}
