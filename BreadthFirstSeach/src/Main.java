5b2fd7090749aaa9bd2f4821cc3416870978583c
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-07-11 20:25:51 +0200

    Added graph data structure and breadth first search and example

---
+++ BreadthFirstSeach/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        //  Breadth First Search = Traverses a graph level by level
+        //                         Utilizes a Queue
+        //                         Better if the destination is on average close to start
+        //                         Siblings are visited before children
+
+        //  Depth First Search = Traverses a graph branch by branch
+        //                       Utilizes a Stack
+        //                       Better if destination is on average far from the start
+        //                       Children are visited before siblings
+        //                       More popular for games/puzzles
+
+        Graph graph = new Graph(5);
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 2);
+
+        graph.print();
+
+        graph.breadthFirstSearch(0);
+
+    }
+}
